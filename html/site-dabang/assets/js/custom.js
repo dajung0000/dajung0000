fbd5892984040907583772ad0630af84fa848549
dajung0000 <65323818+dajung0000@users.noreply.github.com>
2020-05-22 18:48:35 +0900

    2020.05.22

---
+++ html/site-dabang/assets/js/custom.js
@@ -0,0 +1,10 @@
+var mySwiper2 = new Swiper ('#nav1 .swiper-container', {
+   // Optional parameters
+   slidesPerView : 'auto',
+   // If we need pagination
+   scrollbar: {
+     el: '.swiper-scrollbar',
+     dragSize : 152,
+     hide: false,
+   },
+ });
